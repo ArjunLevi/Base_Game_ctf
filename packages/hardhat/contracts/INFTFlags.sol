d4f03584a1afc172b1928dc2021a53f35d121694
ArjunLevi <66naga18@gmail.com>
2026-01-22 12:59:17 +0530

    Season 2 Challenge INFTFlags Updated Solidity file

--- packages/hardhat/contracts/INFTFlags.sol
+++ packages/hardhat/contracts/INFTFlags.sol
@@ -1,10 +1,20 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
 interface INFTFlags {
-    function mint(address _recipient, uint256 _challengeId) external;
+    // Basic ERC721
+    function mint(address to, uint256 challengeId) external;
+    function balanceOf(address owner) external view returns (uint256);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
 
-    function hasMinted(address _recipient, uint256 _challengeId) external view returns (bool);
+    // ERC721Enumerable
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
 
+    // Challenge Logic
+    function tokenIdToChallengeId(uint256 tokenId) external view returns (uint256);
     function tokenIdCounter() external view returns (uint256);
+
+    // Authorization Check (Used by S2 to check S1 progress)
+    function hasMinted(address user, uint256 challengeId) external view returns (bool);
 }
