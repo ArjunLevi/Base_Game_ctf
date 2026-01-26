0bf6c40026024b3920cb2609d8041d95a7ec614f
ArjunLevi <66naga18@gmail.com>
2026-01-26 10:48:48 +0530

    Update the Challenge ts file

---
+++ packages/hardhat/deploy/02_deploy_challenge_2_solution.ts
@@ -0,0 +1,28 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const deploySolution: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  // --- DELETE OR COMMENT OUT THIS LINE ---
+  // const challenge2 = await hre.ethers.getContract("Challenge2");
+
+  // --- ADD THIS LINE ---
+  // PASTE THE REAL ADDRESS FROM THE WEBSITE BELOW
+  const challenge2Address = "0x0b997E0a306c47EEc755Df75fad7F41977C5582d";
+
+  console.log("😈 Attacking Challenge 2 at:", challenge2Address);
+
+  await deploy("S1Challenge2Solution", {
+    from: deployer,
+    args: [challenge2Address], // Use the variable you just created
+    log: true,
+    autoMine: true,
+  });
+};
+
+export default deploySolution;
+deploySolution.tags = ["S1Challenge2Solution"];
+
+// yarn deploy --tags S1Challenge2Solution --network optimism --reset
